5d987debe442fe9d0018ed12e83c3baffbec36b1
Joeri van Dooren <joeri@weepee.org>
2016-01-14 01:29:26 +0100

    auto install wordpress

--- run_wordpress.sh
+++ run_wordpress.sh
@@ -5,5 +5,10 @@ if ! [ -d /tmp/run/httpd ]; then mkdir -p /tmp/run/httpd;fi
 if ! [ -d /tmp/log/httpd ]; then mkdir -p /tmp/log/httpd;fi
 if ! [ -d /tmp/lock/httpd ]; then mkdir -p /tmp/lock/httpd;fi
 
+# install wordpress if not in persistentvolume
+if [ ! -f /var/www/html/index.php ]; then
+  cp -vfpr /wordpress/* /var/www/html/
+fi
+
 #run Apache
 httpd -f /httpd.conf -D FOREGROUND
